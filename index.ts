1d2b96202b84556b78d86918d978c6668d66814f
eddbuildsthings <edward@schauman-haigh.se>
2026-04-06 13:43:45 +0200

    bump ts version

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello via Bun!");
+import { type LLMConfig } from "./types";
+
+const llmConfig =  {
+    vocabSize: 4096,
+    maxSeqLen: 128,
+    dModel: 384,
+    nLayers: 6
+} as LLMConfig
+
+console.log(llmConfig);
---
+++ types.ts
@@ -0,0 +1,8 @@
+export type LLMConfig = {
+    vocabSize:  number
+    maxSeqLen:  number
+    dModel:     number
+    nLayers:    number
+}
+
+export type trainConfig = {}
