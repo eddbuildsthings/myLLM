f5c669981ccba9d559990be46f695317807b203d
eddbuildsthings <edward@schauman-haigh.se>
2026-04-06 13:44:27 +0200

    lint

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import { type LLMConfig } from "./types";
 
 const llmConfig =  {
-    vocabSize: 4096,
-    maxSeqLen: 128,
-    dModel: 384,
-    nLayers: 6
+    vocabSize:  4096,
+    maxSeqLen:  128,
+    dModel:     384,
+    nLayers:    6
 } as LLMConfig
 
 console.log(llmConfig);
